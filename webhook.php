3e44ec60a4119dc1722b36950a2bd0d331e18a0f
patomporn <ch.patomporn_st@tni.ac.th>
2022-03-23 15:05:42 +0700

    test

--- webhook.php
+++ webhook.php
@@ -20,7 +20,10 @@
 	// // $data = gettype($_REQUEST);
 	// fwrite($myfile, $data."\n");
 
-	fwrite($myfile, print_r($_REQUEST,true));
+	fwrite($myfile, print_r($_REQUEST['object'],true));
+	fwrite($myfile, "\n");
+
+	fwrite($myfile, print_r($_REQUEST['data'],true));
 	fwrite($myfile, "\n");
 
 	$dateTime = date('Y-m-d H:i:s');
